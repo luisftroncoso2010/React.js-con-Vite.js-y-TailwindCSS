200db99cc68899cd7a91e76416fd573cc6342057
luisftroncoso2010 <troncober@hotmail.com>
2024-10-17 15:47:27 -0500

    React-Vite-Ecommerse. Consumiendo la API para pintar las card

--- react-vite-ecommerse/src/Pages/Home/index.jsx
+++ react-vite-ecommerse/src/Pages/Home/index.jsx
@@ -1,12 +1,28 @@
+import { useState, useEffect } from "react"
 import Layout from "../../Component/Layout"
 import Card from "../../Component/Card/indesx"
 
 const Home = () => {
+
+    const [items, setItems] = useState(null); 
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+            .then(response => response.json())
+            .then(data => setItems(data))
+      }, [])
+     console.log(items);
+      
+
   return (
     <Layout>
-     Home  
-      
-     <Card />   
+     Home 
+     <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {
+          items?.map((item) => (
+          <Card key={item.id} data={item} />
+          ))     
+        } 
+     </div>
     </Layout>
   )
 }
