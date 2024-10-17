51cb02165105d9e2895f42ac8e7cf07275af3f7c
luisftroncoso2010 <troncober@hotmail.com>
2024-10-17 16:41:28 -0500

    React-Vite-Ecommerse. Contexto Global de la aplicacion

---
+++ react-vite-ecommerse/src/Context/index.jsx
@@ -0,0 +1,14 @@
+import { createContext } from 'react'
+
+const ShoppingCartContext = createContext()
+
+const ShoppingCartProvider = ({ children }) => {
+  return (    
+    <ShoppingCartContext.Provider>
+      {children}    
+    </ShoppingCartContext.Provider>
+  )
+}
+
+export default ShoppingCartProvider
+
